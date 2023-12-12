0ab12dca0ca6d3a6093228231a692d6e4ac6c879
Greg <gregory.navez@std.heh.be>
2023-12-12 11:36:28 +0100

    dev layout inscription basique

---
+++ app/src/main/java/be/heh/pfa/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package be.heh.pfa
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
+    private fun btn_register_onClick(){
+        println("hello from register btn")
+}
